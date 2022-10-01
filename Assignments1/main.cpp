072270f427a5b4f34435e40b2e42b2b5c14c532b
seungwoo <mongoose@likelion.org>
2022-10-01 18:15:43 +0900

    Assignments1
    k-mean cluster algorithm

---
+++ Assignments1/main.cpp
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct {
+    double x;
+    double y;
+    int group;
+    double distance;
+} clust;
+
+int Len;
+clust *Data;
+clust *clustCenterPoint;
+
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+}
+
+/** 초기 중심점 구하기 */
+void initializePoint(int centerCnt) {
+    clust MaxDistancePoint = {0, 0, 0, 0};
+
+    for (int i = 0; i < Len; i++) {
+        Data[i].distance = 1e9;
+        for (int centerIdx = 0; centerIdx < centerCnt; centerIdx++) {
+            double tempDistance = distance(Data[i].x, Data[i].y, clustCenterPoint[centerIdx].x,
+                                           clustCenterPoint[centerIdx].y);
+            Data[i].distance = (Data[i].distance > tempDistance) ? tempDistance : Data[i].distance;
+        }
+        MaxDistancePoint = (MaxDistancePoint.distance > Data[i].distance) ? MaxDistancePoint : Data[i];
+    }
+    clustCenterPoint[centerCnt] = MaxDistancePoint;
+}
+
+/** 군집화 해주기 */
+void clustering(int centerCnt) {
+    for (int i = 0; i < centerCnt; i++) {
+        clustCenterPoint[i].group = 0;
+        clustCenterPoint[i].distance = 0;
+    }
+    for (int idx = 0; idx < Len; idx++) {
+        Data[idx].distance = 1e9;
+        for (int centerIdx = 0; centerIdx < centerCnt; centerIdx++) {
+            double tempDistance = distance(Data[idx].x, Data[idx].y, clustCenterPoint[centerIdx].x,
+                                           clustCenterPoint[centerIdx].y);
+            if (Data[idx].distance > tempDistance) {
+                Data[idx].distance = tempDistance;
+                Data[idx].group = centerIdx;
+            }
+        }
+        clustCenterPoint[Data[idx].group].group += 1;
+        if (clustCenterPoint[Data[idx].group].distance < Data[idx].distance) {
+            clustCenterPoint[Data[idx].group].distance = Data[idx].distance;
+        }
+    }
+}
+
+int sameClustCenterPoint(clust prev, clust current) {
+    if ((prev.x != current.x) || (prev.y != current.y)) {
+        return 0;
+    }
+    return 1;
+}
+
+void relocationCenter(int centerCnt) {
+    for (int i = 0; i < centerCnt; i++) {
+        clustCenterPoint[i].x = 0;
+        clustCenterPoint[i].y = 0;
+    }
+    for (int idx = 0; idx < Len; idx++) {
+        clustCenterPoint[Data[idx].group].x += Data[idx].x;
+        clustCenterPoint[Data[idx].group].y += Data[idx].y;
+    }
+    for (int i = 0; i < centerCnt; i++) {
+        clustCenterPoint[i].x /= clustCenterPoint[i].group;
+        clustCenterPoint[i].y /= clustCenterPoint[i].group;
+    }
+}
+
+void readFile(char *FileName) {
+
+    FILE *fp = fopen(FileName, "r");
+    if (fp == NULL) {
+        printf("Wrong Input : %s\n", FileName);
+        return;
+    }
+    fscanf(fp, "%d", &Len);
+
+    Data = (clust *) malloc(sizeof(clust) * Len);
+    for (int i = 0; i < Len; i++) {
+        fscanf(fp, "%lf %lf", &Data[i].x, &Data[i].y);
+    }
+
+}
+
+void solve() {
+    char *FILENAME = (char *) malloc(sizeof(char) * 10);
+    int clusterNum;
+
+    printf("파일 이름과 k 값을 입력하세요: ");
+    scanf("%s %d", FILENAME, &clusterNum);
+
+    readFile(FILENAME);
+    free(FILENAME);
+
+
+    clustCenterPoint = (clust *) malloc(sizeof(clust) * clusterNum);
+
+    Data[0].group = 1;
+    clustCenterPoint[0] = Data[0];
+    for (int i = 1; i < clusterNum; i++) {
+        initializePoint(i);
+    }
+
+    printf("초기 클러스터의 구성:\n");
+    for (int i = 0; i < clusterNum; i++) {
+        printf("        클러스터 %d 중심점 = (%lf, %lf)\n", i, clustCenterPoint[i].x, clustCenterPoint[i].y);
+    }
+    clustering(clusterNum);
+    int loopCnt = 2;
+    clust *prev = (clust *) malloc(sizeof(clust) * clusterNum);
+    while (1) {
+
+        for (int i = 0; i < clusterNum; i++) {
+            prev[i].x = clustCenterPoint[i].x;
+            prev[i].y = clustCenterPoint[i].y;
+        }
+        relocationCenter(clusterNum);
+
+        int breakPoint = 0;
+        for (int i = 0; i < clusterNum; i++) {
+            breakPoint += sameClustCenterPoint(prev[i], clustCenterPoint[i]);
+        }
+        if(breakPoint == 5) {
+            break;
+        }
+        printf("%d번째 클러스터 구성:\n", loopCnt++);
+        for (int i = 0; i < clusterNum; i++) {
+            printf("        클러스터 %d 중심점 = (%lf, %lf)\n", i, clustCenterPoint[i].x, clustCenterPoint[i].y);
+        }
+        clustering(clusterNum);
+    }
+    printf("### 클러스터 구성 완료!! : 반복 횟수 = %d\n", --loopCnt);
+    for (int i = 0; i < clusterNum; i++) {
+        printf("        클러스터 %d 중심점 = (%lf, %lf), point 수 = %d, 최장 거리 = %lf\n", i, clustCenterPoint[i].x, clustCenterPoint[i].y, clustCenterPoint[i].group, clustCenterPoint[i].distance);
+    }
+
+}
+
+int main() {
+    solve();
+    return 0;
+}
